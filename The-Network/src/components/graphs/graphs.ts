8c83363bc0e9ecb2456a6b155606c720d071430f
michael mariea <mariea.michael@gmail.com>
2020-06-17 11:46:34 -0700

    functionalized graphs and added monthly graph

--- The-Network/src/components/graphs/graphs.ts
+++ The-Network/src/components/graphs/graphs.ts
@@ -10,6 +10,7 @@ import { StockData } from '../../injectables/stockdata';
 export class Graph implements OnInit{
     public returnedStock:any
     chart = [];
+    chart2 = [];
 
     constructor(private StockData:StockData){
 
@@ -18,16 +19,50 @@ export class Graph implements OnInit{
     ngOnInit (){
         
     }
-    handleSearch(searchItem) {
-        const priceArry = [];
-        const dateArry = [];
-        const reversePriceArry = [];
-        const reverseDateArry = [];
-        this.StockData.getStock(searchItem).subscribe((res) => {
+    graph(stock) {
+        this.chart2 = new Chart(stock.id, {
+            type: 'line',
+            data: {
+                labels: stock.Dates,
+                datasets: [{
+                    label: 'Value in USD$',
+                    data: stock.Data,
+                    backgroundColor: [
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)'
+                    ],
+                    borderWidth: 1
+                    }]
+                },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                }}]}}
+                }) 
+    }
+    handleSearch(searchItem){
+        const priceArry = []
+        const dateArry = []
+        const reversePriceArry = []
+        const reverseDateArry = []
+        this.StockData.getStock(searchItem).subscribe((res)=> {
             if (res["Error Message"]){
                 window.alert("Not a real stock name");
             } else {
-                
 
             
             let dailyseries = res["Time Series (Daily)"]
@@ -41,6 +76,16 @@ export class Graph implements OnInit{
             //date arrays
             let weeklyDates = reverseDateArry[0].slice(92, 99)
             let monthlyDates = reverseDateArry[0].slice(69, 99)
+            let weeklyGraphInfo = {
+                Dates: weeklyDates,
+                Data: (reversePriceArry[0].slice(92, 99)),
+                id: 'canvas'
+            }
+            let monthlyGraphInfo = {
+                Dates: monthlyDates,
+                Data: (reversePriceArry[0].slice(69, 99)),
+                id: 'canvas2'
+            }
             //returning Graph Object
             this.returnedStock = {
                 priceArry,
@@ -51,66 +96,11 @@ export class Graph implements OnInit{
                 weeklychangeCheck: Math.sign((reversePriceArry[0][99])-(reversePriceArry[0][92])),
                 monthlychange: ((reversePriceArry[0][99])-(reversePriceArry[0][69])).toString().slice(0,8),
                 monthlychangeCheck: Math.sign((reversePriceArry[0][99])-(reversePriceArry[0][69])),
-                weeklyDates: (weeklyDates),
-                weeklyData: (reversePriceArry[0].slice(92, 99)),
-                monthlyDates: (monthlyDates),
-                monthlyData: (reversePriceArry[0].slice(69, 99)),
                 currentValue: (reversePriceArry[0][99]),
             }
-            //Creating Charts
-            this.chart = new Chart('canvas', {
-                type: 'line',
-                data: {
-                    labels: this.returnedStock.weeklyDates,
-                    datasets: [{
-                        label: 'Value in USD$',
-                        data: this.returnedStock.weeklyData,
-                        backgroundColor: [
-                            'rgba(10, 245, 155, 0.2)',
-                            'rgba(10, 245, 155, 0.2)',
-                            'rgba(10, 245, 155, 0.2)',
-                            'rgba(10, 245, 155, 0.2)',
-                            'rgba(10, 245, 155, 0.2)',
-                            'rgba(10, 245, 155, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(0, 0, 0, 1)',
-                            'rgba(0, 0, 0, 1)',
-                            'rgba(0, 0, 0, 1)',
-                            'rgba(0, 0, 0, 1)',
-                            'rgba(0, 0, 0, 1)',
-                            'rgba(0, 0, 0, 1)'
-                        ],
-                        borderWidth: 1
-                        }]
-                    },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                }}]}}
-                })
+            this.graph(weeklyGraphInfo);
+            this.graph(monthlyGraphInfo);
             }
-        })
-    }
-    graph() {
-        console.log(this.chart, "ah");
-        this.chart = new Chart('canvas', {
-            type: 'line',
-
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-            options: {
-            }
-        })
-    }
+        }
+    )}
 }
