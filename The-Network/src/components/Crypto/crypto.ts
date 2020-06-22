27bd4a797fe47e35a56b85933570302a7e13ac72
michael mariea <mariea.michael@gmail.com>
2020-06-21 19:09:08 -0700

    working crypto search page

--- The-Network/src/components/Crypto/crypto.ts
+++ The-Network/src/components/Crypto/crypto.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CrytoData } from '../../injectables/Cryptodata';
+import { Chart } from 'chart.js';
 
 @Component ({
     selector: 'Crypto',
@@ -7,14 +8,81 @@ import { CrytoData } from '../../injectables/Cryptodata';
     styleUrls: ['./crypto.scss']
 })
 export class Crypto {
-    constructor(private StockData:CrytoData){
+    public cryptoData:any;
+    graphed = false;
+    chart = [];
+    chart2 = [];
+    constructor(private CryptoData:CrytoData){
 
+    }
+    graph(Data){
+        this.graphed = true;
+        this.chart2 = new Chart(Data.id, {
+            type: 'line',
+            data: {
+                labels: Data.Dates,
+                datasets: [{
+                    label: 'Value in USD$',
+                    data: Data.Data,
+                    backgroundColor: [
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)',
+                        'rgba(10, 245, 155, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)',
+                        'rgba(0, 0, 0, 1)'
+                    ],
+                    borderWidth: 1
+                    }]
+                },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                }}]}}
+                }) 
     }
     handleSearch(searchItem){
-        console.log(searchItem)
-        this.StockData.getCrypto(searchItem).subscribe((res) => {
+        let pricesARRY;
+        let dateARRY;
+        let newDATES = [];
+
+        console.log(newDATES);
+        this.CryptoData.getCryptoWeek(searchItem).subscribe((res) => {
             console.log(res);
+            pricesARRY = res['0']['prices'];
+            dateARRY = res['0']['timestamps'];
+            for (let i = 0; i < dateARRY.length; i ++){
+                newDATES.push(dateARRY[i].substring(0,10));
+            }
+            console.log(dateARRY);
+            let weeklyGraphInfo = {
+                Dates: newDATES.slice(32, 39),
+                Data: (pricesARRY.slice(32, 39)),
+                id: 'canvas'
+            }
+            let monthlyGraphInfo = {
+                Dates: newDATES.slice(9, 39),
+                Data: (pricesARRY.slice(9, 39)),
+                id: 'canvas2'
+            }
+            let maxPast = {
+                Dates: newDATES.slice(0, 39),
+                Data: (pricesARRY.slice(0, 39)),
+                id: 'canvas3'
+            }
+            this.graph(weeklyGraphInfo);
+            this.graph(monthlyGraphInfo);
+            this.graph(maxPast);
         })
     }
-
 }
--- The-Network/src/injectables/Cryptodata.ts
+++ The-Network/src/injectables/Cryptodata.ts
@@ -8,18 +8,32 @@ export class CrytoData {
     constructor(private http: HttpClient){
 
     }
-
-    getCrypto(searchItem): Observable<object>{
-        var today = new Date();
-        today.toISOString().substring(0, 10);
+         ISODateString(d){
+        function pad(n){return n<10 ? '0'+n : n}
+        return d.getUTCFullYear()+'-'
+             + pad(d.getUTCMonth()+1)+'-'
+             + pad(d.getUTCDate())+'T'
+             + pad(d.getUTCHours())+':'
+             + pad(d.getUTCMinutes())+':'
+             + pad(d.getUTCSeconds())+'Z'}
+       
+       
+       
+    getCryptoWeek(searchItem): Observable<object>{
+        var d = new Date();
+        var FiftyDaysAgo = new Date();
+        FiftyDaysAgo.setDate(FiftyDaysAgo.getDate() - 40);
         const BASEURL = "https://api.nomics.com/v1/currencies/sparkline?key=";
         const BASEURL2 = "&ids=";
-        const STARTDATE = today + "T00%3A00%3A00Z";
+        const TODAYSDATE = this.ISODateString(d);
+        const STARTDATE = this.ISODateString(FiftyDaysAgo);
+        console.log(STARTDATE);
         const APIKEY = "f93f4af9f98fe883e5fbac5d4d8de8b2";
-        let query = BASEURL + APIKEY + BASEURL2 + searchItem.searchInput + "&start=" + STARTDATE;
+        let query = BASEURL + APIKEY + BASEURL2 + searchItem.searchInput + "&start=" + STARTDATE + "&end=" + TODAYSDATE;
          return this.http.get(query);   
     }
 }
+//
 
 
 // /fetch("&ids=BTC,ETH,XRP&start=2018-04-14T00%3A00%3A00Z&end=2018-05-14T00%3A00%3A00Z")
