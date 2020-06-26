228869be7ed4efa0d17ba41c5f7ea8fc6c349fcf
michael mariea <mariea.michael@gmail.com>
2020-06-26 12:33:01 -0700

    admin functions

--- The-Network/src/components/Stocks/stock-thumbnail.ts
+++ The-Network/src/components/Stocks/stock-thumbnail.ts
@@ -7,7 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 export class StockThumbnailComponent implements OnInit{
     @Input() Stock:any
     stockinfo:any
+    admin: boolean
     ngOnInit(){
-    
+        if (sessionStorage.getItem('username')){
+            this.admin = true;
+        }
+        else {
+            this.admin = false;
+        }
     }
 }
